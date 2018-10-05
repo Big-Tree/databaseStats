44527ebc880a3305aac1f4bf4e94d022ca7861c0
Big-Tree <william.murphy333@gmail.com>
2018-10-05 17:22:00 +0100

    Put main body of script in main()

--- databaseStats.py
+++ databaseStats.py
@@ -10,14 +10,14 @@ import usefulFunctions as uf
 
 # Should be passed ImageSOPIUID of unique lesions only to avoid repeated
 # matches.
-# If unique contrilaterals is set to true then the function will only return a
-# single contrilateral per Image_SOPIUID ensuring that there is at most one
-# contrilateral per patient.
+# If unique contralaterals is set to true then the function will only return a
+# single contralateral per Image_SOPIUID ensuring that there is at most one
+# contralateral per patient.
 def cont_match(properties_to_match, sheet_0,
-                            unique_contrilaterals, Image_SOPIUID):
+                            unique_contralaterals, Image_SOPIUID):
     index = sheet_0['ImageSOPIUID']
     index = index[index == Image_SOPIUID].index[0]
-    # Run through each image in the spreadsheet looking for contrilaterals
+    # Run through each image in the spreadsheet looking for contralaterals
     tmp_properties = dict(properties_to_match)
     matches = []
     for index in range(len(sheet_0['ImageSOPIUID'])):
@@ -26,14 +26,14 @@ def cont_match(properties_to_match, sheet_0,
         if tmp_properties == properties_to_match:
             if sheet_0['ImageSOPIUID'][index] != Image_SOPIUID:
                 # Contrilateral found
-                if unique_contrilaterals == True:
+                if unique_contralaterals == True:
                     return [sheet_0['ImageSOPIUID'][index]]
                 else:
                     matches.append(sheet_0['ImageSOPIUID'][index])
     return matches
 
 
-# Write a funciton that finds all the contrilaterals for a given
+# Write a funciton that finds all the contralaterals for a given
 # ImageSOPIUID
 def mp_get_contralateral(sheet_0, one_per_study, items):
     # Get properties for image:
@@ -57,7 +57,7 @@ def mp_get_contralateral(sheet_0, one_per_study, items):
     for item in lesion_properties:
         lesion_properties[item] = sheet_0[item][index]
 
-    # Set contrilateral properties
+    # Set contralateral properties
     cont_properties = dict(lesion_properties)
     if cont_properties['ImageLaterality'] == 'R':
         cont_properties['ImageLaterality'] = 'L'
@@ -71,82 +71,89 @@ def mp_get_contralateral(sheet_0, one_per_study, items):
 
 
 
-#____________MAIN__________
+def main():
+    pool = mp.Pool()
+    # Get list of excel files
+    spreadsheet_paths = uf.getFiles(
+        '/vol/research/mammo2/will/data/batches/metadata',
+        '*IMAGE.xls')
+    sheet_0 = pd.ExcelFile(spreadsheet_paths[0]).parse(0)
+    sheet_1 = pd.ExcelFile(spreadsheet_paths[0]).parse(1)
 
-pool = mp.Pool()
-# Get list of excel files
-spreadsheet_paths = uf.getFiles('/vol/research/mammo2/will/data/batches/metadata',
-                           '*IMAGE.xls')
-sheet_0 = pd.ExcelFile(spreadsheet_paths[0]).parse(0)
-sheet_1 = pd.ExcelFile(spreadsheet_paths[0]).parse(1)
+    print(len(spreadsheet_paths), ' spreadsheets found')
 
-print(len(spreadsheet_paths), ' spreadsheets found')
+    # Create stats dict
+    stats_template = {'ImageSOPIUID':0,
+                      'StudyIUID':0,
+                      'ROIs':0,
+                      'Unique ROIs':0,
+                      'Unique Contralaterals':0}
+    stats = {}
+    batch_numbers = [1, 3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 19, 21,
+                     22, 23, 30, 31, 32, 33, 40, 42, 43 , 44, 45, 46, 47, 48,
+                     49, 50, 51]
+    for num in batch_numbers:
+        stats.update({'batch ' + str(num): dict(stats_template)})
 
-# Create stats dict
-stats_template = {'ImageSOPIUID':0,
-                  'StudyIUID':0,
-                  'ROIs':0,
-                  'Unique ROIs':0,
-                  'Unique Contrilaterals':0}
-stats = {}
-batch_numbers = [1, 3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 19, 21, 22,
-                 23, 30, 31, 32, 33, 40, 42, 43 , 44, 45, 46, 47, 48, 49, 50,
-                 51]
-for num in batch_numbers:
-    stats.update({'batch ' + str(num): dict(stats_template)})
+    # Load stats into stats
+    for path in spreadsheet_paths:
+        batch = 'batch ' + path.split('_')[1]
+        print('Batch: ', batch)
+        sheet_0 = pd.ExcelFile(path).parse(0)
+        sheet_1 = pd.ExcelFile(path).parse(1)
 
-# Load stats into stats
-for path in spreadsheet_paths:
-    batch = 'batch ' + path.split('_')[1]
-    print('Batch: ', batch)
-    sheet_0 = pd.ExcelFile(path).parse(0)
-    sheet_1 = pd.ExcelFile(path).parse(1)
+        # Get ImageSOPIUID
+        stats[batch]['ImageSOPIUID'] = len(sheet_0['ImageSOPIUID'])
 
-    # Get ImageSOPIUID
-    stats[batch]['ImageSOPIUID'] = len(sheet_0['ImageSOPIUID'])
+        # Get StudyIUID
+        tmp_id = ''
+        for id in sheet_0['StudyIUID'] :
+            if id != tmp_id:
+                stats[batch]['StudyIUID'] += 1
+                tmp_id = id
 
-    # Get StudyIUID
-    tmp_id = ''
-    for id in sheet_0['StudyIUID'] :
-        if id != tmp_id:
-            stats[batch]['StudyIUID'] += 1
-            tmp_id = id
+        # Get ROIs
+        stats[batch]['ROIs'] = len(sheet_1['ImageSOPIUID'])
 
-    # Get ROIs
-    stats[batch]['ROIs'] = len(sheet_1['ImageSOPIUID'])
+        # Get Unique ROIs
+        unique_ROIs = []
+        tmp_ImageSOPIUID = ''
+        for index, ImageSOPIUID in enumerate(sheet_1['StudyIUID']):
+            if ImageSOPIUID != tmp_ImageSOPIUID:
+                unique_ROIs.append(sheet_1['ImageSOPIUID'][index])
+                tmp_ImageSOPIUID = ImageSOPIUID
+                stats[batch]['Unique ROIs'] = len(unique_ROIs)
 
-    # Get Unique ROIs
-    unique_ROIs = []
-    tmp_ImageSOPIUID = ''
-    for index, ImageSOPIUID in enumerate(sheet_1['StudyIUID']):
-        if ImageSOPIUID != tmp_ImageSOPIUID:
-            unique_ROIs.append(sheet_1['ImageSOPIUID'][index])
-            tmp_ImageSOPIUID = ImageSOPIUID
-    stats[batch]['Unique ROIs'] = len(unique_ROIs)
+        # Get contralaterals
+        # Make sure only unique ROIs are used as args
+        func = partial(mp_get_contralateral, sheet_0, True)
+        results = pool.map(
+            func, list(zip(
+                range(len(unique_ROIs)),
+                itertools.repeat(len(unique_ROIs)),
+                unique_ROIs)))
+        stats[batch]['Unique Contralaterals'] = sum(results)
 
-    # Get contrilaterals
-    # Make sure only unique ROIs are used as args
-    func = partial(mp_get_contralateral, sheet_0, True)
-    results = pool.map(
-        func, list(zip(
-            range(len(unique_ROIs)),
-            itertools.repeat(len(unique_ROIs)),
-            unique_ROIs)))
-    stats[batch]['Contrilaterals'] = sum(results)
+        # Get mass classifications
 
-# Total up the stats
-stats_total = dict(stats_template)
-for batch in stats:
-    for single_stat in stats[batch]:
-        stats_total[single_stat] += stats[batch][single_stat]
+    # Total up the stats
+    stats_total = dict(stats_template)
+    for batch in stats:
+        for single_stat in stats[batch]:
+            stats_total[single_stat] += stats[batch][single_stat]
 
-# Print results
-for batch in stats:
-    print(batch)
-    for single_stat in stats[batch]:
-        print('    ', single_stat, ': ', stats[batch][single_stat])
-print('\nTotals')
-for single_stat in stats_total:
-    print('    ', single_stat, ': ', stats_total[single_stat])
-print('Unique ROIs = ', '{:.2f}'.format(
-    stats_total['Unique ROIs'] / stats_total['ImageSOPIUID'] * 100), '%')
+    # Print results
+    for batch in stats:
+        print(batch)
+        for single_stat in stats[batch]:
+            print('    ', single_stat, ': ', stats[batch][single_stat])
+    print('\nTotals')
+    for single_stat in stats_total:
+        print('    ', single_stat, ': ', stats_total[single_stat])
+    print('Unique ROIs = ', '{:.2f}'.format(
+        stats_total['Unique ROIs']
+        / stats_total['ImageSOPIUID']
+        * 100), '%')
+
+if __name__ == '__main__':
+    main()
